242b65289c3bfe996b373baa4227d51ebde2b9c3
francdomain <cfrancmail@gmail.com>
2024-03-07 01:35:06 +0100

    added port number

--- app.py
+++ app.py
@@ -40,4 +40,5 @@ def delete(id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
+    # app.run(debug=True)
