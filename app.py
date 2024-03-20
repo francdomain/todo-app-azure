56b82ac310eadc2ef1f9a82fd92dcc3c186fa1e1
francdomain <cfrancmail@gmail.com>
2024-03-20 20:46:44 +0100

    edited code

--- app.py
+++ app.py
@@ -48,7 +48,5 @@ def delete(id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    # os.environ.get('PORT', 8000) is to dynamically set the port
-    port = int(os.environ.get('PORT', 8000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=8000)
 
